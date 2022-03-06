db243de0c4db789c9df64b3a9e104138c0d62b74
LeeSihyung <67951313+iamjjanga-ouo@users.noreply.github.com>
2022-03-06 17:51:01 +0900

    Fix(entrypoint.sh) : Fix bad loop syntax in sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
-echo "count number...\n"
-for (( c=0; c<=$INPUT_COUNT_NUM; c++ )); then
+echo "count number to ${INPUT_COUNT_NUM}..."
+for (( c=0; c<=$INPUT_COUNT_NUM; c++ ))
 do
     echo $c
 done
